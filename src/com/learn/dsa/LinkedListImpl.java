e2fe07c557ce95ecda3f1978a43861cc42435b33
Abhishek <sharma.abhishek1739@gmail.com>
2022-08-07 23:28:19 +0530

    LinkedList Impl v2.0

--- src/com/learn/dsa/LinkedListImpl.java
+++ src/com/learn/dsa/LinkedListImpl.java
@@ -1,17 +1,15 @@
 package com.learn.dsa;
 
-import com.learn.dsa.helper.LinkedList;
+
+import com.learn.dsa.util.LinkedListUtil;
+
+import java.util.Scanner;
 
 public class LinkedListImpl {
     public static void main(String[] args){
-        LinkedList l = new LinkedList();
-        l.addFirst(10);
-        l.addFirst(20);
-        l.addFirst(30);
-        l.addFirst(40);
-        l.delete(30);
-        l.display();
-
+        Scanner s = new Scanner(System.in);
+        LinkedListUtil util = new LinkedListUtil();
+        util.crudActions();
 
 
 
